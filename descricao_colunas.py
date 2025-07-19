01f5dd8a58214cbbb2a6d3ba313d4d9b4816fb74
Gui-Ferreir4 <gui231112@gmail.com>
2025-07-19 19:00:40 -0300

    Create descricao_colunas.py

---
+++ descricao_colunas.py
@@ -0,0 +1,8 @@
+descricao_colunas = {
+    "time(ms)": "Tempo desde o início (ms)",
+    "ENGI_IDLE": "Motor em marcha lenta",
+    "IC_SPDMTR(km/h)": "Velocidade painel",
+    "VSS(km/h)": "Velocidade roda",
+    "RPM(1/min)": "Rotações por minuto",
+    ...
+}
